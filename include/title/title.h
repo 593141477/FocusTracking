1b1f3bd01b7ecb2a4f20ad29a981017964b1b46b
LazyLie <leopard.lie@gmail.com>
2014-04-12 01:53:08 +0800

    Struct title && title bundle

---
+++ include/title/title.h
@@ -0,0 +1,21 @@
+#ifndef __title_h__
+#define __title_h__
+
+#include <string>
+#include <vector>
+
+struct title {
+    std::wstring name;
+    std::string url;
+    int rank;
+};
+
+struct titlebundle {
+    std::vector< std::wstring > tags;
+    std::vector< title > titles;
+    int size() {
+	return titles.size();
+    }
+};
+
+#endif
