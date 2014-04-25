85854d87210e244dcec6f61cd06d11c748b3e75f
张宇翔 <zz593141477@gmail.com>
2014-04-25 09:50:59 +0800

    set default `date` value

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `CrawlingResult` (
+    `site` varchar(16) NOT NULL, 
     `title` varchar(128) NOT NULL, 
     `url`   varchar(256) NOT NULL, 
     `date`  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX url_idx ON `CrawlingResult`(`url`);
-CREATE INDEX date_idx ON `CrawlingResult`(`date`);
+CREATE INDEX date_idx ON `CrawlingResult`(`date`,`site`);
